b706027d4ba57c1a518eee6cdde5ae689bf78242
SafarSoFar <so_far_away0525@proton.me>
2024-10-17 21:41:54 +0500

    shader usage

--- main.js
+++ main.js
@@ -9,22 +9,40 @@ const renderer = new THREE.WebGLRenderer(); renderer.setSize(window.innerWidth, 
 document.body.appendChild(renderer.domElement);
 
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const shaderMat = new THREE.ShaderMaterial({
-  uniforms: {
-    colorA: { type: 'vec3', value: new THREE.Color(0xff0000) },
-    colorB: { type: 'vec3', value: new THREE.Color(0x0000FF) }
-  },
-  vertexShader: vertexShader(),
-  fragmentShader: fragmentShader()
-});
-const cube = new THREE.Mesh(geometry, shaderMat);
+const geometry = new THREE.OctahedronGeometry(1, 1);
+const mat = new THREE.Material();
+const cube = new THREE.Mesh(geometry, mat);
 scene.add(cube);
 camera.position.z = 5;
 renderer.setAnimationLoop(animate);
 
+
+
+var colorA = new THREE.Color(0x000000);
+var colorB = new THREE.Color(0x000000);
+var r = 0.0;
+var g = 0.0;
+var b = 0.0;
+var glowIn = true;
 function animate() {
-  // cube.rotation.x += 0.01; cube.rotation.y += 0.01;
+
+  r = glowIn ? r + 0.01 : r - 0.01;
+  g = glowIn ? g + 0.01 : g - 0.01;
+  if ((glowIn && r > 1) || (!glowIn && r < 0)) {
+    glowIn = !glowIn;
+  }
+  colorA.r = r;
+  colorB.g = g;
+  const shaderMat = new THREE.ShaderMaterial({
+    uniforms: {
+      colorA: { type: 'vec3', value: colorA },
+      colorB: { type: 'vec3', value: colorB },
+    },
+    vertexShader: vertexShader(),
+    fragmentShader: fragmentShader()
+  });
+  cube.material = shaderMat;
+  cube.rotation.x += 0.01; cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
@@ -49,6 +67,7 @@ function fragmentShader() {
 
       void main() {
         gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+        // gl_FragColor = vec4(colorA, 1.0);
       }
   `
 }
