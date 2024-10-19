4874c9dd75c0d5b4e1b00ae5e421c132feebfb4d
SafarSoFar <so_far_away0525@proton.me>
2024-10-19 12:31:35 +0500

    depth,surface water color mix, color debug

--- main.js
+++ main.js
@@ -6,23 +6,35 @@ import GUI from 'lil-gui';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+
+renderer.setClearColor(0x03b1fc);
+// renderer.setClearColor(0x000000);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var clock = new THREE.Clock();
-const waves = {
-  amplitude: 0.1,
-  freq: 0.01
+const waveObj = {
+  amplitude: 0.15,
+  freq: 2,
+  colorMixOffset: 0.54,
+  // waterDepthColor: 0x0000ff,
+  waterDepthColor: { r: 0, g: 0, b: 1 },
+  // waterSurfaceColor: 0x03b1fc,
+  waterSurfaceColor: { r: 0.078, g: 0.667, b: 0.961 },
+
 };
 const gui = new GUI();
-gui.add(waves, 'amplitude', 0.1, 1.0);
-gui.add(waves, 'freq', 0.01, 0.2);
+gui.add(waveObj, 'amplitude', 0.1, 1.0);
+gui.add(waveObj, 'freq', 2, 10);
+gui.add(waveObj, 'colorMixOffset', 0.2, 2);
+gui.addColor(waveObj, 'waterDepthColor');
+gui.addColor(waveObj, 'waterSurfaceColor');
 
 
-const geometry = new THREE.PlaneGeometry(5, 5, 100, 10);
+const geometry = new THREE.PlaneGeometry(20, 20, 100, 100);
 const mat = new THREE.Material();
-const plane = new THREE.LineSegments(geometry, mat);
+const plane = new THREE.Mesh(geometry, mat);
 
 plane.rotation.x = 5;
 scene.add(plane);
@@ -31,27 +43,24 @@ renderer.setAnimationLoop(animate);
 
 
 
-var colorA = new THREE.Color(0x000000);
-var colorB = new THREE.Color(0x0000ff);
-var angle = 0;
 
 function animate() {
-  angle++;
   const shaderMat = new THREE.ShaderMaterial({
     uniforms: {
-      angle: { type: 'float', value: angle },
-      amplitude: { type: 'float', value: waves.amplitude },
-      freq: { type: 'float', value: waves.freq },
+      amplitude: { type: 'float', value: waveObj.amplitude },
+      freq: { type: 'float', value: waveObj.freq },
       time: { type: 'float', value: clock.getElapsedTime() },
-      colorA: { type: 'vec3', value: colorA },
-      colorB: { type: 'vec3', value: colorB },
+      waterDepthColor: { type: 'vec3', value: waveObj.waterDepthColor },
+      waterSurfaceColor: { type: 'vec3', value: waveObj.waterSurfaceColor },
+      colorMixOffset: { type: 'float', value: waveObj.colorMixOffset },
     },
     vertexShader: vertexShader(),
-    fragmentShader: fragmentShader()
+    fragmentShader: fragmentShader(),
   });
+  shaderMat.transparent = true;
   plane.material = shaderMat;
 
-  plane.rotation.z += 0.005;
+  // plane.rotation.z += 0.005;
 
   renderer.render(scene, camera);
 }
@@ -61,15 +70,18 @@ function animate() {
 function vertexShader() {
   return `
     varying vec3 vUv; 
-    uniform float angle;
     uniform float amplitude;
     uniform float time;
     uniform float freq; 
+    out vec3 pos;
 
     void main() {
       vUv = position; 
-      vec3 pos = position;
-      pos.z = sin(pos.x + pos.y + angle * freq) * amplitude;
+      pos = position;
+      pos.z = sin(freq * pos.x + time) * amplitude;
+      pos.z += cos(freq * pos.x + time) * amplitude;
+      pos.z += sin(freq * pos.y  + time) * amplitude;
+      pos.z += cos(freq * pos.y  + time) * amplitude;
       vec4 modelViewPosition = modelViewMatrix * vec4(pos,1.0);
       gl_Position = projectionMatrix * modelViewPosition;
     }
@@ -78,13 +90,14 @@ function vertexShader() {
 
 function fragmentShader() {
   return `
-      uniform vec3 colorA; 
-      uniform vec3 colorB; 
+      in vec3 pos;
       varying vec3 vUv;
+      uniform float colorMixOffset;
+      uniform vec3 waterDepthColor; 
+      uniform vec3 waterSurfaceColor; 
 
       void main() {
-        gl_FragColor = vec4(mix(colorA, colorB, 0.7), 1.0);
-        // gl_FragColor = vec4(colorA, 1.0);
+        gl_FragColor = vec4(mix(waterDepthColor,waterSurfaceColor, pos.z+colorMixOffset),1.0);
       }
   `
 }
